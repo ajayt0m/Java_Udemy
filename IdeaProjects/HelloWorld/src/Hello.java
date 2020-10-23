bfb294c2c9c986b58899bd6fc6bc0753e8e04479
ajaytom <ajaytom@users.noreply.github.com>
2020-10-23 18:42:39 +0530

    Java Udemy course initial commit

---
+++ IdeaProjects/HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+  public static void main(String[] args){
+    System.out.println("Hai my name is ajay");
+    int myFirstNumber = (10+5) + (2*10);
+    int mySecondNumber = 12;
+    int myThirdNumber = myFirstNumber * 2;
+    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+    int myLastOne = 1000 - myTotal;
+    System.out.println(myLastOne);
+  }
+}
