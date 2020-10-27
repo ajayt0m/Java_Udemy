5cbaddae5f7070709560b51d632cc3ec2a16c9b4
ajaytom <ajaytom@users.noreply.github.com>
2020-10-27 19:38:52 +0530

    PrimitiveTypesChallenge

---
+++ IdeaProjects/PrimitiveTypesChallenge/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            byte myByte = 125;
+            short myShort =1001;
+            int myInt = 100;
+            long myLong = 50000 + (10*(myByte + myShort + myInt));
+    System.out.println(myLong);
+    }
+}
