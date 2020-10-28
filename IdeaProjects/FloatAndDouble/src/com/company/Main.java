9644cd418cddec6d9d40fdc3aee1093ca861e31e
ajaytom <ajaytom@users.noreply.github.com>
2020-10-28 17:14:22 +0530

    Float and double datatypes

---
+++ IdeaProjects/FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float myMinFloatValue = Float.MIN_VALUE;
+	    float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum value = " + myMinFloatValue);
+        System.out.println("Float Maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum value = " + myMinDoubleValue);
+        System.out.println("Double Maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = (float) 5.25;
+        float myFloatValue2 = 5.25f;
+        double myDoubleValue = 5d;
+    }
+}
