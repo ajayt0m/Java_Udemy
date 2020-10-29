b6c8d7db1c8f565299023389afe5769dddc889e2
ajaytom <ajaytom@users.noreply.github.com>
2020-10-29 21:09:12 +0530

    Recap on primitive data types

---
+++ IdeaProjects/Strings/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Eight primitive data types
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+     // String is not a primitive data type
+
+
+    }
+}
