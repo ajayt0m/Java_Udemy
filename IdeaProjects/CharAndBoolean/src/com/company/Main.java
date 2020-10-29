6b9e3e1a89c8d8c82786c30ff6f3ee96fdd78eda
ajaytom <ajaytom@users.noreply.github.com>
2020-10-29 16:19:20 +0530

    boolean type

--- IdeaProjects/CharAndBoolean/src/com/company/Main.java
+++ IdeaProjects/CharAndBoolean/src/com/company/Main.java
@@ -6,7 +6,14 @@ public class Main {
 
         char myChar = 'D';
         char myUnicodeChar = '\u0044';
+        char myCopyrightChar = '\u00A9';
         System.out.println(myChar);
         System.out.println(myUnicodeChar);
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+        System.out.println(isCustomerOverTwentyOne);
     }
 }
