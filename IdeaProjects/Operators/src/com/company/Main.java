a77f8d2773a8224aef09ae22f3d519a6c03e1ced
ajaytom <ajaytom@users.noreply.github.com>
2020-10-31 20:10:21 +0530

    Logical And Operator

--- IdeaProjects/Operators/src/com/company/Main.java
+++ IdeaProjects/Operators/src/com/company/Main.java
@@ -52,5 +52,16 @@ public class Main {
             System.out.println("And I am scared of aliens");
         }
 
+        int topScore = 80;
+        if (topScore < 100)
+        {
+            System.out.println("You got the high score");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100)
+        {
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
