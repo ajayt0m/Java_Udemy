f0cca5d934e102bc02cef00807e6b37f3dd1856d
ajaytom <ajaytom@users.noreply.github.com>
2020-11-01 13:31:11 +0530

    Statements White Space and Indenting

---
+++ IdeaProjects/StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int     myVariable
+                =
+                50;
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test" +
+                " another" +
+                " still more");
+
+        int anotherVariable = 50; myVariable--; System.out.println("Can perform multiple statements in a single line");
+    }
+}
