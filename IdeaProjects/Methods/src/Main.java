0ad85f7fcbc05b64336a5d84aeb2700271817c6a
ajaytom <ajaytom@users.noreply.github.com>
2020-11-01 15:41:26 +0530

    Cleaning up the method

--- IdeaProjects/Methods/src/Main.java
+++ IdeaProjects/Methods/src/Main.java
@@ -2,9 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true, 10000, 8, 200);
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
     }
 
     public static  void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus)
