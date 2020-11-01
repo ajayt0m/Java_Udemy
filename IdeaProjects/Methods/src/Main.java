71093a31613ad579cd308715e84fb2c0c5ab2aa6
ajaytom <ajaytom@users.noreply.github.com>
2020-11-01 16:28:16 +0530

    Method Challenge

--- IdeaProjects/Methods/src/Main.java
+++ IdeaProjects/Methods/src/Main.java
@@ -7,22 +7,29 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your Second Final Score = " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your Second Final Score = " + highScore);
+
+
     }
 
-    public static  void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus)
-    {
-        if (gameOver)
-        {
-            int finalscore =  score + (levelCompleted * bonus);
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalscore = score + (levelCompleted * bonus);
             finalscore += 2000;
-            System.out.println("Your Second Final Score = " + finalscore);
+            return finalscore;
         }
+        return -1; // -1 is conventionally used to indicate an error
     }
+
 }
+
